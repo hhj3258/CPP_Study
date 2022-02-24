e473d3c3f8a96c83b9dc21e2cbdc5472b67ce920
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2022-02-24 20:27:54 +0900

    chapter04_스택 프레임

---
+++ Chapter04_함수/스택_프레임/스택_프레임/스택_프레임.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int MultiplyBy(int a, int b) 
+{
+    int c = a * b;
+    return c;
+}
+
+// F5
+// F10
+// F11
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int result = MultiplyBy(b, a);
+    
+    return 0;
+}
