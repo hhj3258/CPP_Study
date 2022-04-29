f86cf08f4da0421b117a6087b8ed809376da9bd0
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2022-04-29 22:40:54 +0900

    chapter12_delete

---
+++ Chapter12_Morern_Cpp/06_delete/06_delete/06_delete.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+// 오늘의 주제 : delete (삭제된 함수)
+
+class Knight
+{
+public:
+	Knight()
+	{
+
+	}
+
+	// delete 사용방법
+	// 완벽하게 삭제하는 방법
+	// public 영역에 쓰는 게 관례
+	void operator=(const Knight& k) = delete;
+
+private:
+	// 반쪽짜리 삭제 방법
+	Knight(const Knight& rhs);
+	// friend 는 접근이 가능하도록 만들 수도 있다. <- 사실 하면 안됨.
+	friend class Admin;
+};
+
+class Admin
+{
+public:
+	void CopyKnight(const Knight& rhs)
+	{
+		Knight k(rhs);
+	}
+};
+
+int main()
+{
+	Knight k1;
+	Knight k2;
+
+	//k1 = k2;    // Error!
+
+	return 0;
+}
